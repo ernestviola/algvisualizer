ca4cb6ac7a9c86ce98e160a88d5326999bdf11b3
Ernest Viola <ernest.viol@gmail.com>
2022-11-11 18:07:07 -0500

    Added mergesort

--- src/routes/Sortingvisualizer.js
+++ src/routes/Sortingvisualizer.js
@@ -164,8 +164,99 @@ const Sortvisualizer = () => {
 
         setQueue([]);
 
-        
+        // have to do a bottom up approach
 
+        // separate array into groups at intervals of powers of two: 1,2,4,8,16 etc
+
+        var curr_size
+        var left_start
+        var n = newArr.length
+
+        for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+ 
+
+            for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+                // Find ending point of left
+                // subarray. mid+1 is starting
+                // point of right
+                var mid = Math.min(left_start + curr_size - 1, n - 1);
+ 
+                var right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
+ 
+                // Merge Subarrays arr[left_start...mid]
+                // & arr[mid+1...right_end]
+                merge(newArr, left_start, mid, right_end);
+                let tempArr = [...newArr];
+                queue.push([...tempArr]);
+
+            }
+        }
+
+        for (let i = 0; i < newArr.length; i++) {
+            newArr[i] = {value: newArr[i].value,highlight:true}
+            queue.push([...newArr])
+        }
+
+        console.log(queue.length)
+        for (let i = 0; i < queue.length; i++) {
+            setTimeout(() => {
+                setArr([...queue[i]])
+            }, 40 * i)
+        }
+
+    }
+
+    function merge(arr , l , m , r) {
+        var i, j, k;
+        var n1 = m - l + 1;
+        var n2 = r - m;
+ 
+        /* create temp arrays */
+        var L = Array(n1).fill(0);
+        var R = Array(n2).fill(0);
+ 
+        /*
+         * Copy data to temp arrays L and R
+         */
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+ 
+        /*
+         * Merge the temp arrays back into arr[l..r]
+         */
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].value <= R[j].value) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /*
+         * Copy the remaining elements of L, if there are any
+         */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /*
+         * Copy the remaining elements of R, if there are any
+         */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
     useEffect(() => {
